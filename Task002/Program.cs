2467e17f6a59dd4ddeaff13f157157c6dd644d46
SVaMi <vsegda@svami.ru>
2023-12-19 01:52:18 +0300

    Добавил решение второй задачи

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте двумерный массив. 
+// Напишите программу, которая поменяет местами 
+// первую и последнюю строку массива.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        
+        // Сохраняем первую строку во временный массив
+        int[] temp = new int[array.GetLength(1)];
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            temp[i] = array[0, i];
+        }
+        
+        // Переносим последнюю строку в первую
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            array[0, i] = array[array.GetLength(0) - 1, i];
+        }
+        
+        // Переносим временную строку в последнюю
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            array[array.GetLength(0) - 1, i] = temp[i];
+        }
+        
+        // Выводим измененный массив
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
