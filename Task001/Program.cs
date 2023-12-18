ffc271be8c6fc0f4e40842414fa0e157503766c5
SVaMi <vsegda@svami.ru>
2023-12-19 01:44:26 +0300

    Добавил решение первой задачи

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        
+        Console.Write("Введите номер строки: ");
+        int row = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Введите номер столбца: ");
+        int column = Convert.ToInt32(Console.ReadLine());
+        
+        // Проверка, что позиция валидная
+        if (row >= 0 && row < array.GetLength(0) && column >= 0 && column < array.GetLength(1))
+        {
+            Console.WriteLine($"Значение в позиции ({row}, {column}) равно {array[row, column]}");
+        }
+        else
+        {
+            Console.WriteLine("Неверная позиция. В массиве нет элемента с такой позицией.");
+        }
+    }
+}
